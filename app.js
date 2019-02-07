1dd90ca4da75fbd40d6f960ebd0a91567534a483
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-02-07 15:27:00 +0200

    adding body-parser functionality

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000');
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 // adding the public folder 
 app.use(express.static('public'));
@@ -18,3 +22,13 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
+app.post('/new', function(req, res){
+
+    let item = req.body.item;
+
+});
+
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000');
+});

bdcb24c5203cc39a8030a5100aecbc8ccde5e913
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-02-08 22:52:18 +0200

    adding ajax request

--- public/js/style.js
+++ public/js/style.js
@@ -1,3 +1,46 @@
+//const Item = require('../../models/Item');
+
 $(document).ready(function(){
     
+    $('input[type=checkbox]').click(function(){
+        
+        // if the click was made to check the box
+        if($(this).prop('checked')){
+
+            var id = $(this).attr('id');
+            alert('check box with id '+ id +' is checked');
+           
+            $.ajax({
+                type: 'GET',
+                url: '/update',
+                data: id,
+                success: function() {
+                    alert('success');
+                },
+                error: function() {
+                    alert('error');
+                }
+            });
+        }
+
+        // if the click was made to uncheck the box
+        else if(!$(this).prop('checked')) {
+
+            var id = $(this).attr('id');
+            alert('check box with id '+ id +' is unchecked');
+
+            $.ajax({
+                type: 'GET',
+                url: '/update',
+                data: {id:id, state:'unchecked'},
+                success: function() {
+                    alert('success');
+                },
+                error: function() {
+                    alert('error');
+                }
+            });
+        }
+    });
+
 });
